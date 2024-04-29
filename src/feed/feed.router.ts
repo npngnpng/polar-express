40aeb48a45bef3ae52b976160efa7a76370ba5c6
길근우 <kesksjxhx@gmail.com>
2024-04-29 11:51:33 +0900

    🧩 :: feed 단건 조회

--- src/feed/feed.router.ts
+++ src/feed/feed.router.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { createFeed } from './feed.service'
+import { createFeed, queryFeed } from './feed.service'
 
 const router = express.Router()
 
-router.post('/', function (req, res) {
+router.post('/', (req, res) => {
     createFeed(req.body.title, req.body.content)
         .then(message => res.json(message))
+});
+
+router.get('/:id', (req, res) => {
+    queryFeed(Number(req.params.id))
+        .then(response => res.json(response))
 });
 
 export default router;
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -1,8 +1,11 @@
 import prisma from '../prisma/prisma'
+import { HttpError } from '../error/http.error'
 import { Feed } from './feed.model'
 
+const feedRepository = prisma.feed;
+
 export async function createFeed(title: string, content: string) {
-    const feed: Feed = await prisma.feed.create({
+    const feed: Feed = await feedRepository.create({
         data: {
             title: title,
             content: content,
@@ -12,4 +15,20 @@ export async function createFeed(title: string, content: string) {
     return {
         message: `success to create feed ${feed.title}`
     }
+}
+
+export async function queryFeed(id: number) {
+    const feed: Feed | null = await feedRepository.findUnique({
+        where: {
+            id: id,
+        }
+    });
+    if (!feed) {
+        throw new HttpError('feed not found', 404);
+    }
+
+    return {
+        title: feed.title,
+        content: feed.content,
+    };
 }
